69dd53c61295f678f2fb3157b2fa0cd11113ae6d
accubility <joshua.obando98@gmail.com>
2021-01-22 20:49:21 -0500

    Updates

---
+++ precache-manifest.694bfdd608f69715ea9ee2518e333f6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537aa886821276430235c2e80f6e2a95",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "0c943d2b220742147616",
+    "url": "/reactPortfolio/static/js/2.9e62db87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPortfolio/static/js/2.9e62db87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d90a20fad58d6944e9a",
+    "url": "/reactPortfolio/static/js/main.abab4ede.chunk.js"
+  },
+  {
+    "revision": "a3582febd2b51d920657",
+    "url": "/reactPortfolio/static/js/runtime-main.628fc033.js"
+  },
+  {
+    "revision": "74bf666138d00543108f7781aede97e9",
+    "url": "/reactPortfolio/static/media/Devhangout.74bf6661.png"
+  },
+  {
+    "revision": "686f775d1db504a02ebeb569d5b28e11",
+    "url": "/reactPortfolio/static/media/OneStop.686f775d.png"
+  },
+  {
+    "revision": "a3899c49c80ad247ccd789b9588a71b7",
+    "url": "/reactPortfolio/static/media/profilepic.a3899c49.jpg"
+  },
+  {
+    "revision": "11bac60ef2d6fb4eff15ae8f8e734b89",
+    "url": "/reactPortfolio/static/media/stockup.11bac60e.png"
+  }
+]);
